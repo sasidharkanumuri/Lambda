26bd4541eca908c8f5b93c7b8da973efe4881e98
“YOUR <kanumuri1994@gmail.com>
2017-07-24 18:52:14 -0400

    c

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-var time = require('time');
-exports.handler = (event, context, callback) => {
-var currentTime = new time.Date();
-currentTime.setTimezone("America/Los_Angeles");
-callback(null, {
-statusCode: '200',
-body: 'The time in Los Angeles is: ' + currentTime.toString(),
-});
+'use strict';
+
+var url = require('url');
+var target = 'http://localhost:3000';
+
+exports.handler = function(event, context, callback) {
+  var urlObject = url.parse(target);
+  var mod = require(
+    urlObject.protocol.substring(0, urlObject.protocol.length - 1)
+  );
+  console.log('[INFO] - Checking ' + target);
+  var req = mod.request(urlObject, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      console.log('[INFO] - Read body chunk');
+    });
+    res.on('end', function() {
+      console.log('[INFO] - Response end');
+      callback();
+    });
+  });
+
+  req.on('error', function(e) {
+    console.log('[ERROR] - ' + e.message);
+    callback(e);
+  });
+  req.end();
 };
