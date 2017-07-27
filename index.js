3889fbf466b8c2b1408531c355dd0bac3e8e118d
“YOUR <kanumuri1994@gmail.com>
2017-07-27 15:49:07 -0400

    CI/CD

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-'use strict';
-console.log('Loading function');
-var rp = require('request-promise');
-exports.handler = (event, context, callback) => {    
+var http = require('http');
 
-    var options = {
-    uri: 'http://localhost:3000',
-    method: 'POST',
-    body: {
-
-    },
-    json: true 
-};
-
-
-    rp(options).then(function (parsedBody) {
-            console.log(parsedBody);
-        })
-        .catch(function (err) {
-            // POST failed... 
-            console.log(err);
-        });
-
+exports.handler = function (event, context) {
+  http.get('http://localhost:3000', function (result) {
+    console.log('Success, with: ' + result.statusCode);
     context.done(null);
+  }).on('error', function (err) {
+    console.log('Error, with: ' + err.message);
+    context.done("Failed");
+  });
 };
